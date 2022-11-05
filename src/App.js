e8486017625ef6e5109b0c6d0b6b2025df04866a
Onias da Rocha <onias.filho@gmail.com>
2022-11-05 12:24:43 -0400

    creates accordion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "primereact/button";
+import { Dialog } from "primereact/dialog";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [isModalOpen, setIsModalOpen] = useState(false);
+
+	const handleToggle = (e) => {
+		const allC = Array.from(e.target.parentNode.parentNode.classList);
+
+		if (allC.includes("active")) {
+			e.target.parentNode.parentNode.classList.remove("active");
+		} else {
+			e.target.parentNode.parentNode.classList.add("active");
+		}
+
+		console.log(e.target.parentNode.parentNode);
+	};
+
+	return (
+		<div className="app-wrapper">
+			<Dialog visible={isModalOpen} onHide={() => setIsModalOpen(false)}>
+				<div className="modal-wrapper">
+					<div className="accordion active">
+						<h3 className="accordion-title">Why shouldn't we trust atoms?</h3>
+						<p className="accordion-text">They make up everything</p>
+
+						<button className="accordion-toggle" onClick={handleToggle}>
+							<i className="fas fa-chevron-down"></i>
+							<i className="fas fa-times"></i>
+						</button>
+					</div>
+					<div className="accordion">
+						<h3 className="accordion-title">Why shouldn't we trust atoms?</h3>
+						<p className="accordion-text">They make up everything</p>
+
+						<button className="accordion-toggle" onClick={handleToggle}>
+							<i className="fas fa-chevron-down"></i>
+							<i className="fas fa-times"></i>
+						</button>
+					</div>
+					<div className="accordion">
+						<h3 className="accordion-title">Why shouldn't we trust atoms?</h3>
+						<p className="accordion-text">They make up everything</p>
+
+						<button className="accordion-toggle" onClick={handleToggle}>
+							<i className="fas fa-chevron-down"></i>
+							<i className="fas fa-times"></i>
+						</button>
+					</div>
+					<div className="accordion">
+						<h3 className="accordion-title">Why shouldn't we trust atoms?</h3>
+						<p className="accordion-text">They make up everything</p>
+
+						<button className="accordion-toggle" onClick={handleToggle}>
+							<i className="fas fa-chevron-down"></i>
+							<i className="fas fa-times"></i>
+						</button>
+					</div>
+				</div>
+			</Dialog>
+			<Button label="Abrir Popup" onClick={() => setIsModalOpen(true)} />
+		</div>
+	);
 }
 
 export default App;
